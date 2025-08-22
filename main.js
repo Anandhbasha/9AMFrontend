34c9816773b837c110debc033e40d98c075d14d2
Anandhbasha <anadhakm@gmail.com>
2025-08-22 17:34:26 +0530

    js

---
+++ main.js
@@ -0,0 +1,31 @@
+// Hoisting
+// a=10
+// console.log(a);
+
+var a = 10
+// console.log(a);
+var a =50
+a=60
+// var a
+a=70
+// console.log(a);
+
+// {
+//     var c=60
+// }
+// console.log(c);
+
+// console.log(a);
+// console.log(b);
+
+let b=20
+b=50
+// console.log(c);
+// {
+//     let d = 70
+// }
+// console.log(d);
+
+const c = 30
+c=60
+console.log(c);
