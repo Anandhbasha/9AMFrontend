a4d94859d4b8e0978d3aca09fa77b76d6e50c131
Anandhbasha <anadhakm@gmail.com>
2025-08-28 15:38:06 +0530

    datatypes

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 // a=10
 // console.log(a);
 
-var a = 10
+// var a = 10
 // console.log(a);
-var a =50
-a=60
+// var a =50
+// a=60
 // var a
-a=70
+// a=70
 // console.log(a);
 
 // {
@@ -26,6 +26,64 @@ b=50
 // }
 // console.log(d);
 
-const c = 30
-c=60
-console.log(c);
+// const c = 30
+// c=60
+// console.log(c);
+
+// operators
+    // arithmetic Operator
+        // +,-,/,,*,%,++,--
+// ternary operator
+let age =18
+
+console.log(age>18?"Eligible":"Not Eligible");
+
+// data types
+    // primitive
+        // Number
+        let a=10
+        // String
+        let name = "xyz"
+        // Boolean
+        // True or False
+        is_comming = true
+        // undifined
+        let newname ;
+        // Null
+        let x = null
+    // Non premitive
+        // array
+        arr = [10,true,undefined,"names"] //10 = 0 - index
+        arr1 = [10,20,30,40]
+
+        // object
+
+        let person = {
+            name:"xyz",
+            age:25,
+            DOB:"12/12/12",
+            place:"CBE",
+            parents:{
+                dad:"abc",
+                mom:"def",
+                sibling:{
+                    brother:"njkkjkh",
+                    sister:"aknbnbnb"
+                }
+            }
+        }
+        // console.log(typeof(a));
+        console.log(arr);
+        console.log(arr[0]);
+        console.log(arr[1]);
+        console.log(arr[2]);
+        console.log(arr[3]);
+
+        console.log(person);
+        console.log(person.name);
+        console.log(person.parents.sibling.brother);
+        
+        
+        
+        
+        
