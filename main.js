9b62121aeb1cd3786409c31cbb2b942fe022a763
Anandhbasha <anadhakm@gmail.com>
2025-09-03 18:28:19 +0530

    promise

--- main.js
+++ main.js
@@ -1,329 +1,443 @@
-// // Hoisting
-// // a=10
-// // console.log(a);
+// // // Hoisting
+// // // a=10
+// // // console.log(a);
 
-// // var a = 10
-// // console.log(a);
-// // var a =50
-// // a=60
-// // var a
-// // a=70
-// // console.log(a);
+// // // var a = 10
+// // // console.log(a);
+// // // var a =50
+// // // a=60
+// // // var a
+// // // a=70
+// // // console.log(a);
 
-// // {
-// //     var c=60
+// // // {
+// // //     var c=60
+// // // }
+// // // console.log(c);
+
+// // // console.log(a);
+// // // console.log(b);
+
+// // let b=20
+// // b=50
+// // // console.log(c);
+// // // {
+// // //     let d = 70
+// // // }
+// // // console.log(d);
+
+// // // const c = 30
+// // // c=60
+// // // console.log(c);
+
+// // // operators
+// //     // arithmetic Operator
+// //         // +,-,/,,*,%,++,--
+// // // ternary operator
+// // let age =18
+
+// // console.log(age>18?"Eligible":"Not Eligible");
+
+// // // data types
+// //     // primitive
+// //         // Number
+// //         let a=10
+// //         // String
+// //         let name = "xyz"
+// //         // Boolean
+// //         // True or False
+// //         is_comming = true
+// //         // undifined
+// //         let newname ;
+// //         // Null
+// //         let x = null
+// //     // Non premitive
+// //         // array
+// //         arr = [10,true,undefined,"names"] //10 = 0 - index
+// //         arr1 = [10,20,30,40]
+
+// //         // object
+
+// //         let person = {
+// //             name:"xyz",
+// //             age:25,
+// //             DOB:"12/12/12",
+// //             place:"CBE",
+// //             parents:{
+// //                 dad:"abc",
+// //                 mom:"def",
+// //                 sibling:{
+// //                     brother:"njkkjkh",
+// //                     sister:"aknbnbnb"
+// //                 }
+// //             }
+// //         }
+// //         // console.log(typeof(a));
+// //         console.log(arr);
+// //         console.log(arr[0]);
+// //         console.log(arr[1]);
+// //         console.log(arr[2]);
+// //         console.log(arr[3]);
+
+// //         console.log(person);
+// //         console.log(person.name);
+// //         console.log(person.parents.sibling.brother);
+        
+        
+        
+        
+// // function
+
+// // function add(a,b){
+// //     console.log(a+b);    
 // // }
-// // console.log(c);
 
-// // console.log(a);
-// // console.log(b);
+// // add(60,81)
+// // add(70,60)
+// // // add()
+// // // add()
+// // // add()
+// // // add()
+// // // // add()
+// // // Es6
 
-// let b=20
-// b=50
-// // console.log(c);
-// // {
-// //     let d = 70
+// // // arrow function
+// // const arrow = (a,b)=>console.log(a+b);
+// // arrow(5,6)
+// // // IFFI
+// // const IFFI = (function(){
+// //     console.log("IFFI is working");    
+// // })
+// // IFFI()
+// // // anoumous
+// // const ana = function(){}
+// // ana()
+
+// // Statements
+// let age = 20
+// // if(age>13){
+// //     console.log("He is Teenager");    
 // // }
-// // console.log(d);
+// // // if else
+// // if(age>13){
+// //     console.log("He is Teenager");    
+// // }
+// // else{
+// //     console.log("He is Child");    
+// // }
 
-// // const c = 30
-// // c=60
-// // console.log(c);
+// // else if
+// // if(age>13 && age<20){
+// //     console.log("He is Teenager");    
+// // }
+// // else if(age<13){
+// //     console.log("He is child");    
+// // }
+// // else{
+// //     console.log("Adult");    
+// // }
 
-// // operators
-//     // arithmetic Operator
-//         // +,-,/,,*,%,++,--
-// // ternary operator
-// let age =18
+// // nested if
+// // Nationality = "India"
+// // State = "TN"
+// // if(age>22){
+// //     console.log("Eligible to vote");
+// //     if(Nationality=="India"){
+// //         console.log("He is from India");
+// //         if(State === "TN"){
+// //             console.log("he can vote in TN");            
+// //         }
+// //     }
+// // }
 
-// console.log(age>18?"Eligible":"Not Eligible");
+// // switch
 
-// // data types
-//     // primitive
-//         // Number
-//         let a=10
-//         // String
-//         let name = "xyz"
-//         // Boolean
-//         // True or False
-//         is_comming = true
-//         // undifined
-//         let newname ;
-//         // Null
-//         let x = null
-//     // Non premitive
-//         // array
-//         arr = [10,true,undefined,"names"] //10 = 0 - index
-//         arr1 = [10,20,30,40]
+// let today = "Monday"
 
-//         // object
-
-//         let person = {
-//             name:"xyz",
-//             age:25,
-//             DOB:"12/12/12",
-//             place:"CBE",
-//             parents:{
-//                 dad:"abc",
-//                 mom:"def",
-//                 sibling:{
-//                     brother:"njkkjkh",
-//                     sister:"aknbnbnb"
-//                 }
-//             }
-//         }
-//         // console.log(typeof(a));
-//         console.log(arr);
-//         console.log(arr[0]);
-//         console.log(arr[1]);
-//         console.log(arr[2]);
-//         console.log(arr[3]);
-
-//         console.log(person);
-//         console.log(person.name);
-//         console.log(person.parents.sibling.brother);
+// switch(today){
+//     case "Monday":
+//         console.log("Today is Monday");
+//         break
+//     case "Tuesday":
+//         console.log("Today is Tuesday");
+//         break
+//     case "Wednesday":
+//         console.log("Today is Wednesday");
+//         break
+//     case "Thursday":
+//         console.log("Today is Thursday");
+//         break
+//     case "Friday":
+//         console.log("Today is Friday");
+//         break
+//     case "Saturday":
+//         console.log("Today is Saturday");
+//         break
+//     default:
+//         console.log("Today is Sunday");
         
-        
-        
-        
-// function
+// }
+
+
+// // looping Statements
+// // while
+// // let num1 = 10
+
+// // while(num1>num2){
+// //     console.log("While is working");
+// //     num2++    
+// // }
+
+// // // do while
+// // do{
+// //     console.log("Do while is working");    
+// // }while(1>5)
+// // for
+// // for(let num2=0;num1>num2;num2++){
+// //     console.log("For is working");    
+// // }
+
+// let arr1 = [50,545,6546,32154465,5341541,755]
+// // console.log(arr1[0]);
+// // console.log(arr1[1]);
+// // console.log(arr1[2]);
+// // console.log(arr1[3]);
+// // console.log(arr1[4]);
+// // console.log(arr1[5]);
+
+// // arrlength = 6
+// // arr Index = 0,1,2,3,4,5
+
+// // let x = 0
+// // while (x<arr1.length) { //0<6 1<6 6<6
+// //     console.log(arr1[x]);  //   50 545
+// //     ++x //x =0+1 = 1 6
+    
+// // }
+
+// // for(let x=0;x<arr1.length;x++){
+// //     console.log(arr1[x]); 
+// // }
+// // for of
+// // for(let x of arr1){
+// //     console.log(x);    
+// // }
+// // // for in 
+// // for(let x in arr1){
+// //     console.log(x);    
+// //     console.log(arr1[x]);    
+// // }
+// // foreach
+
+// let names = "anandh"
+// // a
+// // an
+// // ana
+// // anan
+// // anandh
+// let temp=""
+// // for(let x of names){
+// //     temp+=x
+// //     console.log(temp);    
+// // }
+
+// for(let x=names.length-1;x>=0;x--){
+//     temp+=names[x]
+//     console.log(temp);   
+// }
+
+// // h
+// // hd
+// // 
+// // anand
+// // anan
+// // ana
+// // an
+// // a
+// arr1.forEach((x)=>console.log(x))
+
+
+// // push
+// let arra2 = [20,44,52,88]
+// arra2.push(100)
+// console.log(arra2);
+// arra2[5] = 500
+// console.log(arra2);
+
+
+// // pop
+// arra2.pop()
+// console.log(arra2);
+
+// // shift
+//     arra2.shift()
+//     console.log(arra2);
+    
+// // unshift
+//     arra2.unshift(600)
+//     console.log(arra2);
+// // indexof
+// console.log(arra2.indexOf(600));
+
+// // includes
+// console.log(arra2.includes(660));
+// // map
+//     arra2.map((item)=>console.log(item))
+// // reduce
+// let total = arra2.reduce((acc,sum)=>acc+sum)//sum=600 acc=0
+// console.log(total);
+
+// // filter
+// console.log(arra2.filter((item)=>item%3==0));
+
+// // sort
+// let array1 = ['a',"c","d","b"]
+// let array2 = [10,20,13,15,16]
+// array1.sort()
+// array2.sort()
+// console.log(array1);
+// console.log(array2);
+// // reverse
+// array1.reverse()
+// array2.reverse()
+// console.log(array1);
+// console.log(array2);
+// // concat
+// let a = [1,2]
+// let b = [2,3]
+// console.log(a.concat(b));
+
+// // find
+// console.log(a.find((x)=>x==10));
+
+// // slice
+// console.log(array1.slice(0,2));
+
+// // splice
+// let fruits = ['apple','kiwi','orange']
+// fruits.splice(1,1,'grapes')
+// console.log(fruits);
+
+
+// // join
+// let words = ["Hello","World"]
+// console.log(words.join(" "));
+
+// // split
+// let newName = "Arun kumar"
+// let newValue = newName.split(" ")
+// console.log(newValue)
+
+// // setTimeout
+// setTimeout(()=>{
+//     console.log("Set timeout is working");    
+// },5000)
+// // setInterval
+// setInterval(()=>{
+//     console.log("Set interval is working");    
+// },2000)
+
+
+// async
+    // promise
+    // async awiat
+    // fetch
+    // trycatch
+
+// let newPromise  = new Promise((resolved,reject)=>{
+//     let bookTicket = false
+//     if(bookTicket){
+//         resolved("Ticket Boooked Sucessfully")
+//     }
+//     else{
+//         reject("Unable to book the ticket")
+//     }
+// })
+
+// newPromise.then((result)=>{
+//     console.log(result) 
+// }).catch((err)=>{console.log(err)})
+
+
+// let friend1 = new Promise((resolved,reject)=>{
+//     let reached = true
+//     if(reached){
+//         resolved("friend1 reached home")
+//     }
+//     else{
+//         reject("Not yet reached")
+//     }
+// })
+// let friend2 = new Promise((resolved,reject)=>{
+//     let reached = true
+//     if(reached){
+//         resolved("friend2 reached home")
+//     }
+//     else{
+//         reject("friend2 Not yet reached")
+//     }
+// })
+// let friend3 = new Promise((resolved,reject)=>{
+//     let reached = false
+//      setTimeout(()=>{
+//     if(reached){       
+//             resolved("friend3 reached home")   }
+  
+//     else{
+//         reject("friend3 Not yet reached")
+//     }
+//       },5000)
+// })
+
+// // Promise.race([friend1,friend2,friend3]).then((res)=>{
+// //     console.log(res) 
+// // }).catch((err)=>{console.log(err)})
+// // Promise.any([friend1,friend2,friend3]).then((res)=>{
+// //     console.log(res) 
+// // }).catch((err)=>{console.log(err)})
+// // Promise.all([friend1,friend2,friend3]).then((res)=>{
+// //     console.log(res) 
+// // }).catch((err)=>{console.log(err)})
+// Promise.allSettled([friend1,friend2,friend3]).then((res)=>{
+//     console.log(res) 
+// }).catch((err)=>{console.log(err)})
+
+
+// return
+
 
 // function add(a,b){
-//     console.log(a+b);    
+//     return a+b    
 // }
 
-// add(60,81)
-// add(70,60)
-// // add()
-// // add()
-// // add()
-// // add()
-// // // add()
-// // Es6
+// console.log(add(150,220));
 
-// // arrow function
-// const arrow = (a,b)=>console.log(a+b);
-// arrow(5,6)
-// // IFFI
-// const IFFI = (function(){
-//     console.log("IFFI is working");    
-// })
-// IFFI()
-// // anoumous
-// const ana = function(){}
-// ana()
-
-// Statements
-let age = 20
-// if(age>13){
-//     console.log("He is Teenager");    
-// }
-// // if else
-// if(age>13){
-//     console.log("He is Teenager");    
-// }
-// else{
-//     console.log("He is Child");    
-// }
-
-// else if
-// if(age>13 && age<20){
-//     console.log("He is Teenager");    
-// }
-// else if(age<13){
-//     console.log("He is child");    
-// }
-// else{
-//     console.log("Adult");    
-// }
-
-// nested if
-// Nationality = "India"
-// State = "TN"
-// if(age>22){
-//     console.log("Eligible to vote");
-//     if(Nationality=="India"){
-//         console.log("He is from India");
-//         if(State === "TN"){
-//             console.log("he can vote in TN");            
+// let fetchData = new Promise((resolved,reject)=>{
+//     fetch('https://jsonplaceholder.typicode.com/posts').then((res)=>{
+//         if(!res.ok){
+//             reject("Unable to connect API")
 //         }
-//     }
-// }
+//         else{
+//             resolved(res.json())            
+//         }    })
+// })
 
-// switch
-
-let today = "Monday"
-
-switch(today){
-    case "Monday":
-        console.log("Today is Monday");
-        break
-    case "Tuesday":
-        console.log("Today is Tuesday");
-        break
-    case "Wednesday":
-        console.log("Today is Wednesday");
-        break
-    case "Thursday":
-        console.log("Today is Thursday");
-        break
-    case "Friday":
-        console.log("Today is Friday");
-        break
-    case "Saturday":
-        console.log("Today is Saturday");
-        break
-    default:
-        console.log("Today is Sunday");
+// fetchData.then((result)=>{console.log(result)})
+// .catch((err)=>console.log(err))
+// async
+let fetchData = async()=>{
+    try{
+        let res = await fetch('https://jsonplaceholder.typicode.com/po')
+    if(!res.ok){
+        throw Error ("Unable to connect")
+    }
+    const data =  await res.json()
+    console.log(data);
+    }
+    catch(err){
+        console.log(err);
         
+    }    
 }
 
-
-// looping Statements
-// while
-// let num1 = 10
-
-// while(num1>num2){
-//     console.log("While is working");
-//     num2++    
-// }
-
-// // do while
-// do{
-//     console.log("Do while is working");    
-// }while(1>5)
-// for
-// for(let num2=0;num1>num2;num2++){
-//     console.log("For is working");    
-// }
-
-let arr1 = [50,545,6546,32154465,5341541,755]
-// console.log(arr1[0]);
-// console.log(arr1[1]);
-// console.log(arr1[2]);
-// console.log(arr1[3]);
-// console.log(arr1[4]);
-// console.log(arr1[5]);
-
-// arrlength = 6
-// arr Index = 0,1,2,3,4,5
-
-// let x = 0
-// while (x<arr1.length) { //0<6 1<6 6<6
-//     console.log(arr1[x]);  //   50 545
-//     ++x //x =0+1 = 1 6
-    
-// }
-
-// for(let x=0;x<arr1.length;x++){
-//     console.log(arr1[x]); 
-// }
-// for of
-// for(let x of arr1){
-//     console.log(x);    
-// }
-// // for in 
-// for(let x in arr1){
-//     console.log(x);    
-//     console.log(arr1[x]);    
-// }
-// foreach
-
-let names = "anandh"
-// a
-// an
-// ana
-// anan
-// anandh
-let temp=""
-// for(let x of names){
-//     temp+=x
-//     console.log(temp);    
-// }
-
-for(let x=names.length-1;x>=0;x--){
-    temp+=names[x]
-    console.log(temp);   
-}
-
-// h
-// hd
-// 
-// anand
-// anan
-// ana
-// an
-// a
-arr1.forEach((x)=>console.log(x))
-
-
-// push
-let arra2 = [20,44,52,88]
-arra2.push(100)
-console.log(arra2);
-arra2[5] = 500
-console.log(arra2);
-
-
-// pop
-arra2.pop()
-console.log(arra2);
-
-// shift
-    arra2.shift()
-    console.log(arra2);
-    
-// unshift
-    arra2.unshift(600)
-    console.log(arra2);
-// indexof
-console.log(arra2.indexOf(600));
-
-// includes
-console.log(arra2.includes(660));
-// map
-    arra2.map((item)=>console.log(item))
-// reduce
-let total = arra2.reduce((acc,sum)=>acc+sum)//sum=600 acc=0
-console.log(total);
-
-// filter
-console.log(arra2.filter((item)=>item%3==0));
-
-// sort
-let array1 = ['a',"c","d","b"]
-let array2 = [10,20,13,15,16]
-array1.sort()
-array2.sort()
-console.log(array1);
-console.log(array2);
-// reverse
-array1.reverse()
-array2.reverse()
-console.log(array1);
-console.log(array2);
-// concat
-let a = [1,2]
-let b = [2,3]
-console.log(a.concat(b));
-
-// find
-console.log(a.find((x)=>x==10));
-
-// slice
-console.log(array1.slice(0,2));
-
-// splice
-let fruits = ['apple','kiwi','orange']
-fruits.splice(1,1,'grapes')
-console.log(fruits);
-
-
-// join
-let words = ["Hello","World"]
-console.log(words.join(" "));
-
-// split
-let newName = "Arun kumar"
-let newValue = newName.split(" ")
-console.log(newValue)
+fetchData()
