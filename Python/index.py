e533e079a97c3a503be3a47340c6bc49e5edf814
Anandhbasha <anadhakm@gmail.com>
2025-09-23 10:52:49 +0530

    operators

--- Python/index.py
+++ Python/index.py
@@ -18,19 +18,82 @@ print(a,b,sep="-") '''
 # tclass = True
 # print(type(tclass))
 
-# swapping
-# a=20
-# b=30
-# c=40
+# # swapping
+# # a=20
+# # b=30
+# # c=40
 
-# temp = a
+# # temp = a
 
-# a=b  # a=30
+# # a=b  # a=30
 
-# b=c # b=40
+# # b=c # b=40
 
-# c=temp #c=20
+# # c=temp #c=20
 
-a,b,c=20,30,40
-c,a,b = a,b,c
-print(a,b,c)
+# # a,b,c=20,30,40
+# # c,a,b = a,b,c
+# # print(a,b,c)
+
+
+# # complex Datatypes
+# # list
+# # arr = [50,60,80,66,44] #arr[0] = 50 arr[1] = 60 arr[2] = 80 arr[3] = 66 arr[4] = 44
+# # # print(arr)
+# # print(arr[0])
+# # print(arr[1])
+
+# # tuple
+# x=(101,102,103,104,105)
+# print(x)
+
+# # set
+# newsets = {201,201,203,202,204,202,205}
+# print(newsets)
+
+# # dictionary
+# person = {
+#     "name":"xyz",
+#     "age":24,
+#     "city":"CBE",
+#     "family":{
+#         "dad":"hghjjk",
+#         "mom":"hghjghj",
+#         "siblings":{
+#             "brother":"grfcfd",
+#             "sister":"puyygbnb"
+#         }
+#     }
+# }
+# print(person["family"])
+
+# operators
+# arithmetic Opeartors
+# +,-,*,/,%
+num1=20
+num2=10
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/num2)
+# print(num1//num2)
+# print(num1%num2)
+
+# # comparison operator
+# # >,<,>=,<=,==
+# print(num1>num2)
+# print(num1<num2)
+# print(num1<=20)
+# print(num1>=20)
+# print(num1==20)
+
+# Logical Opearators
+# AND
+# OR
+# NOT
+
+print(num1>num2 and num1==20 and num1<num2)
+print(num1>num2 or num1==20 or num1<num2)
+
+print(not num1==20)
+
