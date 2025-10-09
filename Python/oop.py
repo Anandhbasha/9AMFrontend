44b75b5a3df7cc9cd9d944e3b76358c11875ab75
Anandhbasha <anadhakm@gmail.com>
2025-10-09 11:47:46 +0530

    oop

--- Python/oop.py
+++ Python/oop.py
@@ -69,13 +69,71 @@
 #     print("Child Class")
 
 
-class vechicle:
-    def brake(self):
-        print("Vehicle Stops")
-class car(vechicle):
-    def acc(self):
-        print("Car moves")
+# class vechicle:
+#     def brake(self):
+#         print("Vehicle Stops")
+# class car(vechicle):
+#     def acc(self):
+#         print("Car moves")
     
-c = car()
-c.brake()
-c.acc()
+# c = car()
+# c.brake()
+# c.acc()
+
+
+
+# polymorphism
+# method overriding
+class res:
+    def placeorder(self):
+        print("Order placed from res")
+class vegRes:
+    def placeorder(self):
+        print("Placed order from veg res")
+class NonvegRes:
+    def placeorder(self):
+        print("Placed order from NonvegRes")
+
+r = res()
+vr = vegRes()
+nv = NonvegRes()
+r.placeorder()
+vr.placeorder()
+nv.placeorder()
+
+
+# overloading
+class calculator:
+    def add(self,a=0,b=0,c=0,d=0):
+        print(a+b+c+d)
+
+cal = calculator()
+cal.add(20,50,60,50)
+cal.add(20,50,60)
+cal.add(20,50)
+cal.add(10)
+# abstraction
+from abc import ABC,abstractmethod
+
+class Bank:
+    @abstractmethod
+    def loan(self):
+        pass
+    @abstractmethod
+    def intressst(self):
+        pass
+class Axis(Bank):
+    def loan(self):
+        print("Get a 50000 loan from Axis Bank")
+    def intrest(self):
+        print("Axis Bank Intrest rate is 8.8%")
+class HDFC(Bank):
+    def loan(self):
+        print("Get a 80000 loan from HDFC Bank")
+    def intrest(self):
+        print("Axis Bank Intrest rate is 9.8%")
+
+
+
+obj1 = Axis()
+obj2 = HDFC()
---
+++ Python/pypacksnw.py
@@ -0,0 +1,6 @@
+# pip install numpy
+import numpy as np
+
+arr1=np.array([10,20,30])
+
+
